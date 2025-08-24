bbe461454dd8656886093eb267e82188acb04635
LautaroColman04 <lautacolman04@gmail.com>
2025-08-24 19:07:17 -0300

    ... Entrega 1

---
+++ js/init.js
@@ -0,0 +1,59 @@
+const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
+const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
+const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
+const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
+const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
+const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+const EXT_TYPE = ".json";
+
+let showSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
+
+let hideSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+let getJSONData = function(url){
+    let result = {};
+    showSpinner();
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner();
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner();
+        return result;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+let usuario = localStorage.getItem("usuarioLogueado");
+  if (usuario) {
+  let userDropdownContainer = document.getElementById("userDropdownContainer");
+  let userName = document.getElementById("userName");
+    userDropdownContainer.style.display = "block";
+    userName.textContent = usuario;
+    document.getElementById("logoutBtn").addEventListener("click", function () {
+      localStorage.removeItem("usuarioLogueado");
+      window.location.href = "login.html";
+    });
+  }
+});
+
+if (!localStorage.getItem('usuarioLogueado')) {
+    window.location.href = 'login.html';
+}
